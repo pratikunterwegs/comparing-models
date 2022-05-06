3eceddaf4c405b43183d9a5f456f20b53545ba5b
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-06 19:15:17 +0200

    data types declr and def

---
+++ src/data_types.cpp
@@ -0,0 +1,79 @@
+
+#include "data_types.h"
+
+// function to update gendata
+void genData::updateGenData (Population &pop, const int g_) {
+    
+    int i = g_ / increment;
+    // get social network measures
+    // std::vector<float> measures = pop.pbsn.ntwkMeasures();
+    
+    // get pop data
+    gIntake[i] = pop.intake; // this returns the intake! not the net energy
+    gEnergy[i] = pop.energy; // this returns the net energy, fitness proxy
+    gSF[i] = pop.sF;
+    gSH[i] = pop.sH;
+    gSN[i] = pop.sN;
+    gX[i] = pop.initX;
+    gY[i] = pop.initY;
+    gXn[i] = pop.coordX;
+    gYn[i] = pop.coordY;
+    gAssoc[i] = pop.associations;
+    gMoved[i] = pop.moved;
+
+    gens[i] = g_;
+}
+
+// function to return gen data as an rcpp list
+Rcpp::List genData::getGenData() {
+    Rcpp::List gDataList (gSampled);
+    for (int i = 0; i < gSampled; i++)
+    {
+        gDataList[i] = DataFrame::create(
+            Named("intake") = gIntake[i],
+            Named("energy") = gEnergy[i],
+            Named("sF") = gSF[i],
+            Named("sH") = gSH[i],
+            Named("sN") = gSN[i],
+            Named("x") = gX[i],
+            Named("y") = gY[i],
+            Named("xn") = gXn[i],
+            Named("yn") = gYn[i],
+            Named("assoc") = gAssoc[i],
+            Named("moved") = gMoved[i]
+        );
+    }
+    List dataToReturn = List::create(
+        Named("pop_data") = gDataList,
+        Named("gens") = gens
+    );
+
+    return dataToReturn;
+}
+
+void moveData::updateMoveData(Population &pop, const int t_) {
+    assert(t_ <= tmax && "too many timesteps logged");
+
+    timesteps[t_] = std::vector<int> (popsize, t_);
+    x[t_] = pop.coordX;
+    y[t_] = pop.coordY;
+
+}
+
+Rcpp::List moveData::getMoveData() {
+    Rcpp::List mDataList (tmax);
+    std::vector<int> id(popsize, 0);
+    std::iota(std::begin(id), std::end(id), 0);
+
+    for (int i = 0; i < tmax; i++)
+    {
+        mDataList[i] = DataFrame::create(
+            Named("time") = timesteps[i],
+            Named("x") = x[i],
+            Named("y") = y[i],
+            Named("id") = id
+        );
+    }
+
+    return mDataList;
+}
---
+++ src/data_types.h
@@ -0,0 +1,86 @@
+#ifndef DATA_TYPES_H
+#define DATA_TYPES_H
+/// data types and related functions
+#include <vector>
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include "agents.h"
+#include "network.h"
+
+// define a struct holding a vector of data frames which holds generation wise data
+struct genData {
+public:
+
+    genData(const int genmax, const int popsize, const int increment) :
+        genmax (genmax),
+        increment (increment),
+        gSampled (genmax / increment),
+        // generation wise dataframe
+        // energy
+        gIntake (gSampled, std::vector<float>(popsize, 0.f)),
+        gEnergy (gSampled, std::vector<float>(popsize, 0.f)),
+        // weights
+        gSF (gSampled, std::vector<float>(popsize, 0.f)),
+        gSH (gSampled, std::vector<float>(popsize, 0.f)),
+        gSN (gSampled, std::vector<float>(popsize, 0.f)),
+        // initial positions
+        gX (gSampled, std::vector<float>(popsize, 0.f)),
+        gY (gSampled, std::vector<float>(popsize, 0.f)),
+        gXn (gSampled, std::vector<float>(popsize, 0.f)),
+        gYn (gSampled, std::vector<float>(popsize, 0.f)),
+        // ecological interactions
+        gAssoc (gSampled, std::vector<int>(popsize, 0.f)),
+        gMoved (gSampled, std::vector<float>(popsize, 0.f)),
+        // generation specific data
+        gens (gSampled, 0)
+    {}
+    ~genData() {}
+
+    const int genmax;
+    const int increment;
+    const int gSampled;
+    std::vector<std::vector<float> > gIntake;
+    std::vector<std::vector<float> > gEnergy;
+    std::vector<std::vector<float> > gSF;
+    std::vector<std::vector<float> > gSH;
+    std::vector<std::vector<float> > gSN;
+
+    std::vector<std::vector<float> > gX;
+    std::vector<std::vector<float> > gY;
+    std::vector<std::vector<float> > gXn;
+    std::vector<std::vector<float> > gYn;
+
+    std::vector<std::vector<int> > gAssoc;
+    std::vector<std::vector<float> > gMoved;
+
+    std::vector<int> gens;
+
+    void updateGenData (Population &pop, const int g_);
+    Rcpp::List getGenData ();
+};
+
+struct moveData {
+public:
+
+    moveData(const int tmax, const int popsize) :
+        tmax(tmax),
+        popsize(popsize),
+        timesteps(tmax, std::vector<int>(popsize, 0)),
+        x(tmax, std::vector<float>(popsize, 0)),
+        y(tmax, std::vector<float>(popsize, 0))
+    {}
+    ~moveData() {}
+
+    const int tmax;
+    const int popsize;
+    std::vector<std::vector<int> > timesteps;
+    std::vector<std::vector<float> > x;
+    std::vector<std::vector<float> > y;
+
+    void updateMoveData (Population &pop, const int t_);
+    Rcpp::List getMoveData();
+};
+
+#endif  //
