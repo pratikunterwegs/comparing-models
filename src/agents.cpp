63096a7320ac31fb431dac7329e7492b80a869a6
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-06 19:14:36 +0200

    random move fun

--- src/agents.cpp
+++ src/agents.cpp
@@ -393,6 +393,80 @@ void Population::move_mechanistic(const Resources &food, const int nThreads) {
     
 }
 
+// function to move randomly
+void Population::move_random(const Resources &food) {
+    float twopi = 2.f * M_PI;
+    
+    // what increment for 3 samples in a circle around the agent
+    float increment = twopi / n_samples;
+    float angle = 0.f;
+    // for this increment what angles to sample at
+    std::vector<float> sample_angles (static_cast<int>(n_samples), 0.f);
+    for (int i_ = 0; i_ < static_cast<int>(n_samples); i_++)
+    {
+        sample_angles[i_] = angle;
+        angle += increment;
+    }
+
+    for (int i = 0; i < nAgents; ++i) {
+        int id = order[i];
+        if (counter[id] > 0) {
+            counter[id] --;
+        }
+        else {
+            // first assess current location
+            float sampleX = coordX[id];
+            float sampleY = coordY[id]; 
+
+            float suit_origin = noise(rng);
+
+            float newX = sampleX;
+            float newY = sampleY;
+            // now sample at three locations around
+            for(size_t j = 0; j < sample_angles.size(); j++) {
+                float t1_ = static_cast<float>(cos(sample_angles[j]));
+                float t2_ = static_cast<float>(sin(sample_angles[j]));
+                
+                // use range for agents to determine sample locs
+                sampleX = coordX[id] + (range_perception * t1_);
+                sampleY = coordY[id] + (range_perception * t2_);
+
+                // crudely wrap sampling location
+                if((sampleX > food.dSize) | (sampleX < 0.f)) {
+                    sampleX = std::fabs(std::fmod(sampleX, food.dSize));
+                }
+                if((sampleY > food.dSize) | (sampleY < 0.f)) {
+                    sampleY = std::fabs(std::fmod(sampleY, food.dSize));
+                }
+
+                float suit_dest = noise(rng);
+
+                if (suit_dest > suit_origin) {
+                    // where does the individual really go
+                    newX = coordX[id] + (range_perception * t1_);
+                    newY = coordY[id] + (range_perception * t2_);
+
+                    // crudely wrap MOVEMENT location
+                    if((newX > food.dSize) | (newX < 0.f)) {
+                        newX = std::fabs(std::fmod(newX, food.dSize));
+                    }
+                    if((newY > food.dSize) | (newY < 0.f)) {
+                        newY = std::fabs(std::fmod(newY, food.dSize));
+                    }
+
+                    assert(newX < food.dSize && newX > 0.f);
+                    assert(newY < food.dSize && newY > 0.f);
+                    suit_origin = suit_dest;
+                }
+            }
+            // distance to be moved
+            moved[id] += range_perception;
+
+            // set locations
+            coordX[id] = newX; coordY[id] = newY;
+        }
+    }
+}
 
 // function to paralellise choice of forage item
 void Population::pickForageItem(const Resources &food, const int nThreads){
