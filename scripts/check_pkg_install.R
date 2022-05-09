ea5de718b8bc4c8fa55c0c1d8a45fb480ff5d0bb
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-09 16:50:02 +0200

    script to check package

--- scripts/check_pkg_install.R
+++ scripts/check_pkg_install.R
@@ -5,20 +5,77 @@ devtools::load_all()
 # devtools::install()
 devtools::document()
 
+library(ggplot2)
+
+# test case 0
 a = linkmoveecoevo::model_case_2(
   scenario = 0,
   landsize = 60,
-  tmax = 20,
-  handling_time = 5,
-  dispersal = 3,
-  popsize = 50,
-  nClusters = 100,
-  genmax = 1,
-  regen_time = 100,
-  mProb = 0.01,
   nItems = 1800,
   clusterSpread = 1.0,
+  nClusters = 100,
+  regen_time = 100,
+  tmax = 400,
+  handling_time = 5,
+  popsize = 500,
+  genmax = 1,
+  dispersal = 3,
   range_perception = 1.0,
-  nThreads = 1,
-  mSize = 0.01
+  mProb = 0.01,
+  mSize = 0.01,
+  nThreads = 1
 )
+
+# test case 2
+a = linkmoveecoevo::model_case_2(
+  scenario = 2,
+  landsize = 60,
+  nItems = 1800,
+  clusterSpread = 1.0,
+  nClusters = 100,
+  regen_time = 100,
+  tmax = 20,
+  handling_time = 5,
+  popsize = 500,
+  genmax = 100,
+  dispersal = 10,
+  range_perception = 1.0,
+  mProb = 0.01,
+  mSize = 0.01,
+  nThreads = 2
+)
+
+# test case 1
+a = linkmoveecoevo::model_case_2(
+  scenario = 1,
+  landsize = 60,
+  nItems = 1800,
+  clusterSpread = 1.0,
+  nClusters = 100,
+  regen_time = 100,
+  tmax = 20,
+  handling_time = 5,
+  popsize = 500,
+  genmax = 1,
+  dispersal = 3,
+  range_perception = 1.0,
+  mProb = 0.01,
+  mSize = 0.01,
+  nThreads = 2
+)
+
+str(a)
+
+# plot displacement
+ggplot(a@trait_data)+
+  geom_segment(
+    aes(
+      x = x, y = y,
+      xend = xn, yend = yn
+    )
+  )
+
+b = make_network(a, 2)
+
+plot_network(b) +
+  scale_fill_viridis_c()
