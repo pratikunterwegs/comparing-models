97bea4f1fc790bf5e0e41eadf45b5ad9f037145b
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-09 12:34:36 +0200

    no more generation data

--- src/data_types.h
+++ src/data_types.h
@@ -9,57 +9,7 @@
 #include "agents.h"
 #include "network.h"
 
-// define a struct holding a vector of data frames which holds generation wise data
-struct genData {
-public:
 
-    genData(const int genmax, const int popsize, const int increment) :
-        genmax (genmax),
-        increment (increment),
-        gSampled (genmax / increment),
-        // generation wise dataframe
-        // energy
-        gIntake (gSampled, std::vector<float>(popsize, 0.f)),
-        gEnergy (gSampled, std::vector<float>(popsize, 0.f)),
-        // weights
-        gSF (gSampled, std::vector<float>(popsize, 0.f)),
-        gSH (gSampled, std::vector<float>(popsize, 0.f)),
-        gSN (gSampled, std::vector<float>(popsize, 0.f)),
-        // initial positions
-        gX (gSampled, std::vector<float>(popsize, 0.f)),
-        gY (gSampled, std::vector<float>(popsize, 0.f)),
-        gXn (gSampled, std::vector<float>(popsize, 0.f)),
-        gYn (gSampled, std::vector<float>(popsize, 0.f)),
-        // ecological interactions
-        gAssoc (gSampled, std::vector<int>(popsize, 0.f)),
-        gMoved (gSampled, std::vector<float>(popsize, 0.f)),
-        // generation specific data
-        gens (gSampled, 0)
-    {}
-    ~genData() {}
-
-    const int genmax;
-    const int increment;
-    const int gSampled;
-    std::vector<std::vector<float> > gIntake;
-    std::vector<std::vector<float> > gEnergy;
-    std::vector<std::vector<float> > gSF;
-    std::vector<std::vector<float> > gSH;
-    std::vector<std::vector<float> > gSN;
-
-    std::vector<std::vector<float> > gX;
-    std::vector<std::vector<float> > gY;
-    std::vector<std::vector<float> > gXn;
-    std::vector<std::vector<float> > gYn;
-
-    std::vector<std::vector<int> > gAssoc;
-    std::vector<std::vector<float> > gMoved;
-
-    std::vector<int> gens;
-
-    void updateGenData (Population &pop, const int g_);
-    Rcpp::List getGenData ();
-};
 
 struct moveData {
 public:
