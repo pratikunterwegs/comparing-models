2f2f15b1852757ac7036aac22db0402fd5ab5bf2
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-09 15:50:42 +0200

    add move strategy to network nodes

--- R/fun_make_network.R
+++ R/fun_make_network.R
@@ -2,10 +2,11 @@
 #' Get network data from `simulation_output` objects.
 #'
 #' @param object A `simulation_output` object.
+#' @param weight_threshold Weight below which edges are excluded.
 #'
 #' @return A `tidygraph` objects.
 #' @export
-make_network = function(object) {
+make_network = function(object, weight_threshold) {
     assertthat::assert_that(
         class(object) == "simulation_output",
         msg = "pmv get traits: object is not simulation output"
@@ -16,7 +17,10 @@ make_network = function(object) {
     colnames(edgelist) = c("from", "to", "weight")
     edgelist = data.table::as.data.table(edgelist)
     
-    edgelist = edgelist[from != to, ]
+    edgelist = edgelist[edgelist$from != edgelist$to, ]
+
+    edgelist = edgelist[edgelist$weight >= weight_threshold, ]
+
     edgelist$to = edgelist$to + 1
     edgelist$from = edgelist$from + 1
 
@@ -24,6 +28,19 @@ make_network = function(object) {
     nodes = object@trait_data
     nodes = data.table::as.data.table(nodes)
     nodes$id = seq(nrow(nodes))
+
+    # get movement strategy
+    nodes[, c("sF", "sH", "sN") := lapply(
+      .SD, function(tr_) {
+        tr_ / (abs(sF) + abs(sH) + abs(sN))
+      }
+    ), .SDcols = c("sF", "sH", "sN")][]
+    nodes[, social_strat := data.table::fcase(
+        (sH > 0 & sN > 0), "agent tracking",
+        (sH > 0 & sN < 0), "handler tracking",
+        (sH < 0 & sN > 0), "non-handler tracking",
+        (sH < 0 & sN < 0), "agent avoiding"
+  )][]
 
     # make tidygraph objects
     g = tidygraph::tbl_graph(
