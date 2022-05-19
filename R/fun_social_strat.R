7fdfcb91e40978b597fda1b7bac75512884b3d9b
Pratik Gupte <pratikgupte16@gmail.com>
2022-05-19 16:51:46 +0200

    add social strategy fun

---
+++ R/fun_social_strat.R
@@ -0,0 +1,21 @@
+#' Assign a social strategy
+#'
+#' @param df The dataframe with agent id and social weights.
+#'
+#' @return A dataframe with social strategy assigned.
+#' @export
+#' @import data.table
+#'
+get_social_strategy = function(df) {
+  assertthat::assert_that(
+    all(c("sH", "sN") %in% names(df)),
+    msg = "get_social_strat: data does not have social weights"
+  )
+  data.table::setDT(df)
+  df[, social_strat := data.table::fcase(
+    (sH > 0 & sN > 0), "agent tracking",
+    (sH > 0 & sN < 0), "handler tracking",
+    (sH < 0 & sN > 0), "non-handler tracking",
+    (sH < 0 & sN < 0), "agent avoiding"
+  )][]
+}
