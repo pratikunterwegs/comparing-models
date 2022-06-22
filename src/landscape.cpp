292448e0faedc3ae7afadb34fdb9fd79c1f26025
Pratik Gupte <pratikgupte16@gmail.com>
2022-06-22 14:38:27 +0200

    landscape slot in resources class

--- src/landscape.cpp
+++ src/landscape.cpp
@@ -79,6 +79,17 @@ void Resources::regenerate() {
     countAvailable();
 }
 
+/// function to return a landscape object as an Rcpp dataframe
+Rcpp::DataFrame Resources::record_landscape() {
+    return (
+        Rcpp::DataFrame::create(
+            Named("x") = coordX,
+            Named("y") = coordY,
+            Nmaed("t_avail") = counter
+        )
+    )
+}
+
 /// function to export landscape as matrix
 //' Returns a test landscape.
 //'
