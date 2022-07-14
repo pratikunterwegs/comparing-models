f434374a5c94736424c503004d2b17ac752e290e
Pratik Gupte <pratikgupte16@gmail.com>
2022-07-14 14:06:24 +0200

    update chck code

--- scripts/check_pkg_install.R
+++ scripts/check_pkg_install.R
@@ -11,27 +11,7 @@ devtools::document()
 
 library(ggplot2)
 
-# test case 0
 a <- ecoevomove2::model_case_2(
-  landsize = 60,
-  nItems = 1800,
-  clusterSpread = 1.0,
-  nClusters = 100,
-  regen_time = 100,
-  tmax = 400,
-  handling_time = 5,
-  popsize = 100,
-  genmax = 1,
-  dispersal = 3,
-  range_perception = 1.0,
-  mProb = 0.01,
-  mSize = 0.01,
-  nThreads = 2
-)
-
-# test case 3
-a <- ecoevomove2::model_case_2(
-  scenario = 3,
   landsize = 60,
   nItems = 1800,
   clusterSpread = 1.0,
@@ -40,46 +20,8 @@ a <- ecoevomove2::model_case_2(
   tmax = 20,
   handling_time = 5,
   popsize = 500,
-  genmax = 20,
-  dispersal = 10,
-  range_perception = 1.0,
-  mProb = 0.01,
-  mSize = 0.01,
-  nThreads = 2
-)
-
-# test case 2
-a <- ecoevomove2::model_case_2(
-  scenario = 2,
-  landsize = 60,
-  nItems = 1800,
-  clusterSpread = 1.0,
-  nClusters = 100,
-  regen_time = 100,
-  tmax = 20,
-  handling_time = 5,
-  popsize = 500,
-  genmax = 20,
-  dispersal = 10,
-  range_perception = 1.0,
-  mProb = 0.01,
-  mSize = 0.01,
-  nThreads = 2
-)
-
-# test case 1
-a <- ecoevomove2::model_case_2(
-  scenario = 1,
-  landsize = 60,
-  nItems = 1800,
-  clusterSpread = 1.0,
-  nClusters = 100,
-  regen_time = 100,
-  tmax = 20,
-  handling_time = 5,
-  popsize = 500,
-  genmax = 1,
-  dispersal = 3,
+  genmax = 100,
+  dispersal = 0.01,
   range_perception = 1.0,
   mProb = 0.01,
   mSize = 0.01,
